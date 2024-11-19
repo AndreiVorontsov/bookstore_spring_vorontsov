bc9833e5bb1038b5644bdcbe247199364b251bc7
Andrei Vorontsov <andreivorontsov3@gmail.com>
2024-11-19 15:03:39 +0300

    AppListener - Context initialized

--- src/main/java/com/vorontsov/bookstore/controller/AppListener.java
+++ src/main/java/com/vorontsov/bookstore/controller/AppListener.java
@@ -1,24 +1,35 @@
 package com.vorontsov.bookstore.controller;
 
+import com.vorontsov.bookstore.AppConfig;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @WebListener
 @Log4j2
 public class AppListener implements ServletContextListener {
 
+    private static AnnotationConfigApplicationContext CONTEXT;
+
+    public static AnnotationConfigApplicationContext getContext() {
+        return CONTEXT;
+    }
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-       CommandFactory instance = CommandFactory.INSTANCE;
-        log.info("Context initialized" +instance.hashCode());
+        CONTEXT = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println("Context initialized");
+//       CommandFactory instance = CommandFactory.INSTANCE;
+        log.info("Context initialized"); //+instance.hashCode());
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         log.debug("ServletContextDestroy Event");
-        CommandFactory.INSTANCE.close();
+        CONTEXT.close();
+//        CommandFactory.INSTANCE.close();
         log.info("Context destroyed");
     }
 }
