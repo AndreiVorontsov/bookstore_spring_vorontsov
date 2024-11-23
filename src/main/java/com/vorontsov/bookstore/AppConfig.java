01445f888654311fa80200079869b6f32cac5889
Andrei Vorontsov <andreivorontsov3@gmail.com>
2024-11-23 12:57:22 +0300

    AppConfig add jdbcTempLate(dataSource) and dataSource().

--- src/main/java/com/vorontsov/bookstore/AppConfig.java
+++ src/main/java/com/vorontsov/bookstore/AppConfig.java
@@ -1,16 +1,52 @@
 package com.vorontsov.bookstore;
 
 import com.vorontsov.bookstore.controller.FrontController;
+import com.zaxxer.hikari.HikariDataSource;
 import org.eclipse.tags.shaded.org.apache.xalan.lib.sql.ConnectionPool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath:/application.properties")
 public class AppConfig {
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.user}")
+    private String user;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.driver}")
+    private String driver;
+
+    @Value("${db.poolSize}")
+    private int poolSize;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        HikariDataSource hikari = new HikariDataSource();
+        hikari.setJdbcUrl(url);
+        hikari.setUsername(user);
+        hikari.setPassword(password);
+        hikari.setDriverClassName(driver);
+        hikari.setMaximumPoolSize(poolSize);
+        return hikari;
+    }
 
 }
