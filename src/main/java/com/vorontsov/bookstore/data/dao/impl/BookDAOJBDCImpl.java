0a3c128c5e4d1702f42752576808b72c6699cde1
Andrei Vorontsov <andreivorontsov3@gmail.com>
2024-11-24 17:15:43 +0300

    BookDAOJBDCImpl.softDeleteById to NamedParameterJdbcTemplate BookDAOJBDCImpl.findByAuthor to NamedParameterJdbcTemplate BookDAOJBDCImpl.findByIsbn to NamedParameterJdbcTemplate BookDAOJBDCImpl.countAll to NamedParameterJdbcTemplate

--- src/main/java/com/vorontsov/bookstore/data/dao/impl/BookDAOJBDCImpl.java
+++ src/main/java/com/vorontsov/bookstore/data/dao/impl/BookDAOJBDCImpl.java
@@ -1,6 +1,5 @@
 package com.vorontsov.bookstore.data.dao.impl;
 
-import com.vorontsov.bookstore.data.connection.DataSource;
 import com.vorontsov.bookstore.data.dao.BookDAO;
 import com.vorontsov.bookstore.data.entity.Book;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +9,10 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,13 +30,9 @@ public class BookDAOJBDCImpl implements BookDAO {
     private static final String GET_BY_ISBN_SQL = "select id,name,author,Isbn,cover,Price,Year_Publication,delete from books where ISBN = ?";
     private static final String DEL_BY_ID_SQL = "DELETE FROM books where id = ?";
     private static final String GET_BY_AUTHOR_SQL = "select id,name,author,Isbn,cover,Price,Year_Publication,delete from books where Author = ?";
-    private static final String UPDATE_SQL = "update books set name = ?, author = ?, isbn = ?, cover = ?,price = ?,Year_Publication = ? where id = ?";
     private static final String UPDATE_NP_SQL = "update books set name = :name, author = :author, isbn = :isbn, cover = :cover,price = :price,Year_Publication = :Year_Publication where id = :id";
-    private static final String SOFT_DELETE_SQL = "update books set delete = ? where id = ?";
     private static final String SOFT_DELETE_NP_SQL = "update books set delete = :delete where id = :id";
     private static final String GET_COUNT_ALL_SQL = "select count(*) from books";
-    //private static final String GET_INFO_COLUMNS_SQL = "select collation_name from information_schema.columns where table_name = 'books' and column_name = ?";
-    private final DataSource dataSource;
     private final JdbcTemplate template;
     private final NamedParameterJdbcTemplate namedTemplate;
 
@@ -73,34 +65,6 @@ public class BookDAOJBDCImpl implements BookDAO {
        return template.queryForObject(GET_BY_ID_SQL,this::mapRow,id);
     }
 
-    private Book mapRow(ResultSet rs, int num) throws SQLException {
-        Book book = new Book();
-        book.setId(rs.getLong("id"));
-        book.setName(rs.getString("name"));
-        book.setAuthor(rs.getString("author"));
-        book.setIsbn(rs.getString("Isbn"));
-        book.setCover(Cover.valueOf(rs.getString("cover")));
-        book.setPrice(rs.getBigDecimal("Price"));
-        book.setYearPublication(rs.getInt("Year_Publication"));
-        book.setDelete(rs.getBoolean("delete"));
-        log.debug(book);
-        return book;
-    }
-
-    private Book process(ResultSet resultSet) throws SQLException {
-        Book book = new Book();
-        book.setId(resultSet.getLong("id"));
-        book.setName(resultSet.getString("name"));
-        book.setAuthor(resultSet.getString("author"));
-        book.setIsbn(resultSet.getString("Isbn"));
-        book.setCover(Cover.valueOf(resultSet.getString("cover")));
-        book.setPrice(resultSet.getBigDecimal("Price"));
-        book.setYearPublication(resultSet.getInt("Year_Publication"));
-        book.setDelete(resultSet.getBoolean("delete"));
-        log.debug(book);
-        return book;
-    }
-
     @Override
     public Book update(Book book) {
         Map<String, Object> map = new HashMap<>();
@@ -124,70 +88,39 @@ public class BookDAOJBDCImpl implements BookDAO {
 
     @Override
     public boolean softDeleteById(long id, boolean bool) {
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(SOFT_DELETE_SQL);
-            statement.setBoolean(1, bool);
-            statement.setLong(2, id);
-            log.debug("softDelete book with ID = " + id);
-            statement.executeUpdate();
-            Book book = new Book();
-            return bool == getById(id).isDelete();
-        } catch (SQLException e) {
-            log.error("softDelete book with ID = " + id);
-            throw new RuntimeException(e);
-        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("delete", bool);
+        map.put("id", id);
+        namedTemplate.update(SOFT_DELETE_NP_SQL, map);
+        return bool == getById(id).isDelete();
     }
-
 
     @Override
     public Book findByIsbn(String isbn) {
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(GET_BY_ISBN_SQL);
-            statement.setString(1, isbn);
-            log.debug("Find by isbn " + isbn +":" );
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return process(resultSet);
-            }
-        } catch (SQLException e) {
-            log.error("Find by isbn " + isbn);
-            throw new RuntimeException(e);
-        }
-        return null;
+        return template.queryForObject(GET_BY_ISBN_SQL,this::mapRow,isbn);
     }
 
     @Override
     public List<Book> findByAuthor(String author) {
-        List<Book> books = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(GET_BY_AUTHOR_SQL);
-            statement.setString(1, author);
-            log.debug("Find by author " + author +":" );
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                books.add(process(resultSet));
-            }
-        } catch (SQLException e) {
-            log.error("Find by author " + author);
-            throw new RuntimeException("book Author:" + author, e);
-        }
-        return books;
+        return template.query(GET_BY_AUTHOR_SQL,this::mapRow,author);
     }
 
     @Override
     public long countAll() {
-        try(Connection connection = dataSource.getConnection()) {
-        PreparedStatement statement = connection.prepareStatement(GET_COUNT_ALL_SQL);
-        if (statement.execute()){
-            log.debug("countAll");
-            ResultSet resultSet = statement.getResultSet();
-            resultSet.next();
-            return resultSet.getLong(1);
-        }
-    }catch (SQLException e) {
-            log.error(" Get countAll - ERROR");
-        throw new RuntimeException(e);
+       return template.queryForObject(GET_COUNT_ALL_SQL, Integer.class);
     }
-        throw new RuntimeException("");
+
+    private Book mapRow(ResultSet rs, int num) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getLong("id"));
+        book.setName(rs.getString("name"));
+        book.setAuthor(rs.getString("author"));
+        book.setIsbn(rs.getString("Isbn"));
+        book.setCover(Cover.valueOf(rs.getString("cover")));
+        book.setPrice(rs.getBigDecimal("Price"));
+        book.setYearPublication(rs.getInt("Year_Publication"));
+        book.setDelete(rs.getBoolean("delete"));
+        log.debug(book);
+        return book;
     }
 }
